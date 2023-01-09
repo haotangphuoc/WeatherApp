cf608da5d062ee2f7660e3231d2d630e4234dd8f
Hao Tang <61295938+haotangphuoc@users.noreply.github.com>
2023-01-08 23:16:12 -0400

    Add random img as background and edit the layout

--- script.js
+++ script.js
@@ -2,86 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.querySelector("#search-button")
     const api = "794c4d3ff89210cbb46e105c1d0e3efd"
     const searchBar = document.querySelector("#search-bar")
-    const futureWrapper = document.querySelector("#future")
-    const mapWrapper = document.querySelector('#map')
     const today = new Date()
     currHour = today.getHours() 
+    //Default value for weather forcast is at Halifax
+    displayInfo("Halifax", api)
 
     searchButton.addEventListener("click", () => {
-        const city = searchBar.value
-
-        //Felching data for geocoding
-        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api}`)
-        .then(response => response.json())
-        .then(data => {
-            country = data[0].country
-            lat = data[0].lat
-            lon = data[0].lon
-            
-            //Display city and country
-            document.querySelector("#location-display").innerHTML = `${firstUpperCase(city)}, ${country}`
-
-            //Fetching current weather data
-            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`)
-            .then(response => response.json())
-            .then(data => {
-                // Field variables 
-                temp = (data.main.temp - 273.15).toPrecision(2)
-                weatherDesc = firstUpperCase(data.weather[0].description)
-                weatherMain = data.weather[0].main
-                wind = (data.wind.speed * 3.6).toPrecision(2)
-                humid = data.main.humidity
-
-                //Display current weather 
-                document.querySelector("#current-temp").innerHTML = `${temp}° C`
-                document.querySelector("#current-weather").innerHTML = `${displayWeatherIcon(weatherMain)} ${weatherDesc}`
-                document.querySelector("#current-wind").innerHTML = `Wind: ${wind} km/h`
-                document.querySelector("#current-humid").innerHTML = `Humidity: ${humid} %`
-            })
-
-            //Fetching future weather data
-            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api}`)
-            .then(response => response.json())
-            .then(data => {
-                //Section of data the current hour belong
-                section = Math.round(currHour/3)
-                for(let i = section; i < section + 4; i++) {
-                    //Create new element
-                    furInfo = document.createElement('div')
-                    furInfo.className = 'future-info'
-                    furHeading = document.createElement('div')
-                    furIcon = document.createElement('div')
-                    furWeather = document.createElement('div')
-
-                    //Asign variable to json value
-                    weatherMain = data.list[i].weather[0].main
-                    timeDate = (data.list[i].dt_txt).split(' ')
-                    time = timeDate[1].substring(0,5)
-    
-                    //Display future weather
-                    furHeading.innerHTML = `${time}`
-                    furHeading.className = 'future-time'
-                    furIcon.innerHTML = `${displayWeatherIcon(weatherMain)}`
-                    furIcon.className = 'future-icon'
-                    furWeather.innerHTML = `${weatherMain}`
-                    furWeather.className = 'future-weather'
-
-                    //Add created elements to future weather wrapper
-                    furInfo.appendChild(furHeading)
-                    furInfo.appendChild(furIcon)
-                    furInfo.appendChild(furWeather)
-                    futureWrapper.appendChild(furInfo)
-                }
-                
-            })
-
-            //Fetching map 
-            fetch(`https://tile.openweathermap.org/map/precipitation_new/3/2/2.png?appid=${api}`)
-            .then(response)
-            .then(data => {
-                mapWrapper.innerHTML = data
-            })
-        })
+        city = searchBar.value
+        displayInfo(city, api)
     })
 })  
 
@@ -108,3 +36,79 @@ function displayWeatherIcon(code) {
     }
 }
 
+function displayInfo(city, api) {
+    const futureWrapper = document.querySelector("#future")
+    const mapWrapper = document.querySelector('#map')
+
+    //Display background as a random img
+    body = document.querySelector("body")
+    body.style.backgroundImage = 'url("https://picsum.photos/2500/1500?grayscale")'
+
+    //Felching data for geocoding
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api}`)
+    .then(response => response.json())
+    .then(data => {
+        country = data[0].country
+        lat = data[0].lat
+        lon = data[0].lon
+
+        //Display city and country name
+        document.querySelector('#location-display').innerHTML = `${city}, ${country}`
+
+        //Fetching current weather data
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`)
+        .then(response => response.json())
+        .then(data => {
+            // Field variables 
+            temp = (data.main.temp - 273.15).toPrecision(2)
+            weatherDesc = firstUpperCase(data.weather[0].description)
+            weatherMain = data.weather[0].main
+            wind = (data.wind.speed * 3.6).toPrecision(2)
+            humid = data.main.humidity
+
+            //Display current weather 
+            document.querySelector("#current-temp").innerHTML = `${temp}° C`
+            document.querySelector("#current-weather").innerHTML = `${displayWeatherIcon(weatherMain)} ${weatherDesc}`
+            document.querySelector("#current-wind").innerHTML = `Wind: ${wind} km/h`
+            document.querySelector("#current-humid").innerHTML = `Humidity: ${humid} %`
+        })
+
+        //Fetching future weather data
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api}`)
+        .then(response => response.json())
+        .then(data => {
+            futureWrapper.innerHTML = ''
+
+            //Section of data the current hour belong
+            section = Math.round(currHour/3)
+            for(let i = section; i < section + 4; i++) {
+                //Create new element
+                furInfo = document.createElement('div')
+                furInfo.className = 'future-info'
+                furHeading = document.createElement('div')
+                furIcon = document.createElement('div')
+                furWeather = document.createElement('div')
+
+                //Asign variable to json value
+                weatherMain = data.list[i].weather[0].main
+                timeDate = (data.list[i].dt_txt).split(' ')
+                time = timeDate[1].substring(0,5)
+
+                //Display future weather
+                furHeading.innerHTML = `${time}`
+                furHeading.className = 'future-time'
+                furIcon.innerHTML = `${displayWeatherIcon(weatherMain)}`
+                furIcon.className = 'future-icon'
+                furWeather.innerHTML = `${weatherMain}`
+                furWeather.className = 'future-weather'
+
+                //Add created elements to future weather wrapper
+                furInfo.appendChild(furHeading)
+                furInfo.appendChild(furIcon)
+                furInfo.appendChild(furWeather)
+                futureWrapper.appendChild(furInfo)
+            }
+            
+        })
+    })
+}
